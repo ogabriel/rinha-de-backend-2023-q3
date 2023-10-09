ab00ec4d0b400ad383087315ef7f0f6d19a449b5
ogabriel <gabrieloliver8991@gmail.com>
2023-10-09 20:33:20 -0300

    remove ranadom

--- stress-test/user-files/simulations/rinhabackend/RinhaBackendSimulation.scala
+++ stress-test/user-files/simulations/rinhabackend/RinhaBackendSimulation.scala
@@ -58,18 +58,18 @@ class RinhaBackendSimulation
   setUp(
     criacaoEConsultaPessoas.inject(
       constantUsersPerSec(2).during(10.seconds), // warm up
-      constantUsersPerSec(5).during(15.seconds).randomized, // are you ready?
-      
+      constantUsersPerSec(5).during(15.seconds), // are you ready?
+
       rampUsersPerSec(6).to(600).during(3.minutes) // lezzz go!!!
     ),
     buscaPessoas.inject(
       constantUsersPerSec(2).during(25.seconds), // warm up
-      
+
       rampUsersPerSec(6).to(100).during(3.minutes) // lezzz go!!!
     ),
     buscaInvalidaPessoas.inject(
       constantUsersPerSec(2).during(25.seconds), // warm up
-      
+
       rampUsersPerSec(6).to(40).during(3.minutes) // lezzz go!!!
     )
   ).protocols(httpProtocol)
